298ad74440b6445a338639a3b8b051d99beee265
Yohan <yohancaballeroo@gmail.com>
2026-01-22 09:40:02 +0800

    Added file structure

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-red-500 text-white p-6 text-2xl">
-      If this is red, Tailwind works
-    </div>
-  );
-}
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+  <div>
+    <p className="text-red-500 bg-blue-950">Hellows</p>
+  </div>
+  );
+}
