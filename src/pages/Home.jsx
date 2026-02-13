cfd46c3b4af80aaa846c688d3bb829cf5a6e6dfb
faith-solverous <faithcalma07@gmail.com>
2026-02-13 12:01:37 +0800

    Home page second-fifth section initial layout done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/navbar/navbar";
+import { Link } from 'react-router-dom'; // Make sure you have react-router-dom installed
 
 export default function Home() {
   return (
@@ -49,36 +50,153 @@ export default function Home() {
       </section>
 
       {/* Trusted by our customers & clients */}
-      <section id="" className="flex bg-[#EBF5FD] justify-center items-center text-center p-10">
+      <section id="" className="flex bg-[#EBF5FD] justify-center items-center text-center py-10">
         {/* Title and description */}
-        <div className=" ml-[7.438rem]">
-          <div className="flex flex-row justify-center items-center space-x-1">
-            <h3>Trusted by Our</h3> 
-            <h3>Customers & Partners</h3>
-          </div>
-          <p>We collaborate with recognized technology partners to ensure quality, reliability, and up-to-date ICT solutions.</p>
-        </div>
-
-        {/* Partners Logo (Should be scrollable horizontally) */}
-        <div>
-          {/* Image container (white bg in the design) */}
+        <div className="mx-[7.438rem]">
           <div>
-            {/* Image itself */}
-            <img></img>
+            <div className="flex flex-row justify-center items-center space-x-1">
+              <h3>Trusted by Our</h3>
+              <h3>Customers & Partners</h3>
+            </div>
+            <p>We collaborate with recognized technology partners to ensure quality, reliability, and up-to-date ICT solutions.</p>
+          </div>
+
+          {/* Partners Logo (Should be scrollable horizontally) */}
+          <div className="w-full h-7">
+            {/* Image container (white bg in the design) */}
+            <div>
+              {/* Image itself */}
+              <img></img>
+            </div>
           </div>
         </div>
       </section>
 
       {/* Services We Provide to Elevate Your Business */}
-      <section id="">
-        
+      <section id="" className="flex flex-col bg-white justify-center items-center text-center py-10">
+        <div className="mx-[7.438rem]">
+          <div>
+            <div className="flex flex-row justify-center items-center space-x-1">
+              <h3>Services We Provide to</h3>
+              <h3>Elevate Your Business</h3>
+            </div>
+            <p>MGKK Information Communication Technology Services delivers end-to-end ICT and engineering solutions alongside comprehensive training programs designed to meet industry demands and support digital transformation.</p>
+          </div>
+
+          {/* Solutions (Scrollable horizontally from left to right) */}
+          <div className="flex flex-row text-left">
+            {/* Title, description, buttons left and right */}
+            <div>
+              <div>
+                <h5>Solutions</h5>
+                <h4>End-to-End ICT & Engineering Solutions</h4>
+                <p>We provide ICT consultancy and engineering solutions focused on planning, building, operating, optimizing, and testing technology systems.</p>
+              </div>
+              <div>
+                <button></button>
+                <button></button>
+              </div>
+            </div>
+
+            <div>
+              <h4>ICT Consultancy</h4>
+              <p>MGKK provides ICT consultancy services using the Plan, Build, Operate, Optimize, and Test (PBOOT) approach to support the design, implementation, and improvement of ICT infrastructures.</p>
+              <Link to="">See More</Link>
+            </div>
+
+            <div>
+              <h4>Web and Software Development</h4>
+              <p>We develop web and software solutions that support organizational requirements, using modern technologies to deliver reliable and functional digital systems.</p>
+              <Link to="">See More</Link>
+            </div>
+          </div>
+
+          {/* Training (Scrollable horizontally from right to left) */}
+          <div className="flex flex-row-reverse text-right">
+            {/* Title, description, buttons left and right */}
+            <div>
+              <div>
+                <h5>Training</h5>
+                <h4>ICT Training & Skills Development</h4>
+                <p>We offers face-to-face and online training programs designed to equip individuals and organizations with cutting-edge technological knowledge and practical skills.</p>
+              </div>
+              <div>
+                <button></button>
+                <button></button>
+              </div>
+            </div>
+
+            <div>
+              <h4>Cybersecurity Training</h4>
+              <p>Our cybersecurity training covers cybersecurity fundamentals, ethical hacking, vulnerability assessment and penetration testing (VAPT), and risk management.</p>
+              <Link to="">See More</Link>
+            </div>
+
+            <div>
+              <h4>Cloud Computing Training</h4>
+              <p>We provide cloud computing training on platforms such as AWS, Microsoft Azure, and Google Cloud through face-to-face and online programs.</p>
+              <Link to="">See More</Link>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* Why Choose MGKK ICT Services */}
-      <section></section>
+      <section id="" className="flex flex-row bg-[#EBF5FD] justify-center items-center text-center py-10">
+        {/* Title and description */}
+        <div className="mx-[7.438rem] flex flex-row">
+          <div>
+            <div>
+              <div className="flex flex-row justify-center items-center space-x-1">
+                <h3>Why Choose</h3>
+                <h3>MGKK ICT Services</h3>
+              </div>
+              <p>We collaborate with recognized technology partners to ensure quality, reliability, and up-to-date ICT solutions.</p>
+            </div>
+
+            {/* Image handshaking */}
+            <div><img></img></div>
+          </div>
+
+          <div className="text-left">
+            <div>
+              <h4>Experience</h4>
+              <p>Over a decade of providing ICT and engineering solutions and professional training since our establishment in 2013.</p>
+            </div>
+            <div>
+              <h4>Expertise</h4>
+              <p>Led by a Professional Electronics Engineer and supported by certified engineers, technicians, and instructors with strong industry and academic backgrounds.</p>
+            </div>
+            <div>
+              <h4>Quality Training</h4>
+              <p>We offer face-to-face and online training programs aligned with industry standards and globally recognized certifications.</p>
+            </div>
+            <div>
+              <h4>End-to-End Approach</h4>
+              <p>We follow the Plan, Build, Operate, Optimize, and Test (PBOOT) methodology to ensure structured and effective delivery of ICT solutions.</p>
+            </div>
+            <div>
+              <h4>Innovation & Integrity</h4>
+              <p>Our work is guided by core values that emphasize innovation, ethical practices, excellence, collaboration, and continuous improvement.</p>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* Our Working Progress */}
-      <section></section>
+      <section>
+        <div className="mx-[7.438rem] p-4">
+          <div className="text-right">
+            <div className="flex flex-row justify-end items-center space-x-1">
+              <h3>Our</h3>
+              <h3>Working Progress</h3>
+            </div>
+            <p>MGKK Information Communication Technology Services delivers end-to-end ICT and engineering solutions alongside comprehensive training programs designed to meet industry demands and support digital transformation.</p>
+          </div>
+
+
+        </div>
+      </section>
 
       {/* Our Featured Solutions and Training */}
       <section></section>
