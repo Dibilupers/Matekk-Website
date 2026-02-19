dfdeba96d417b587eb4ab5d9bd73af84abe00fa9
faith-solverous <faithcalma07@gmail.com>
2026-02-19 16:36:49 +0800

    Adjust spacing in Trusted by Our customers section for improved layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -247,7 +247,7 @@ export default function Home() {
 
       {/* Trusted by our customers & clients */}
       <section className="flex bg-[#EBF5FD] justify-center items-center text-center py-10">
-        <div className="mx-[2.063rem] md:mx-16 space-y-5">
+        <div className="mx-[2.063rem] md:mx-16 space-y-4 md:space-y-7">
           <div>
             <div className="flex flex-col md:flex-row justify-center items-center space-x-2 font-bold text-3xl">
               <h3>Trusted by Our</h3>
