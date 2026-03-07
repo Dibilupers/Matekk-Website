7776332c7a16b0e676b421bf9441d6e81cf8d204
adryandevi <doggerman33@example.com>
2026-03-07 13:46:46 +0800

    Added FInished Training Pages in CCNP Enterprise, CCNP Datacenter, CCNP Security, 300-710-SNCF-v1.1, 300-715-SISE-v1.1, 300-730-SVPN-v1.1, and 300-745-SDSI-v1.1

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,6 +6,12 @@ import Footer from "../components/footer/footer";
 import CCNA from "../pages/training/cisco/ccna/ccna";
 import CCNAAutomation from "../pages/training/cisco/ccna/automation";
 import CCNPEnterprise from "../pages/training/cisco/ccnp/enterprise";
+import CCNPSecurity from "../pages/training/cisco/ccnp/security";
+import CCNPDatacenter from "../pages/training/cisco/ccnp/datacenter";
+import SNCF from "../pages/training/cisco/ccnp/ccnp-security/300-710-SNCF-v1.1";
+import SISE from "../pages/training/cisco/ccnp/ccnp-security/300-715-SISE-v1.1";
+import SVPN from "../pages/training/cisco/ccnp/ccnp-security/300-730-SVPN-v1.1";
+import SDSI from "../pages/training/cisco/ccnp/ccnp-security/300-745-SDSI-v1.1";
 import Network from "../pages/solutions/ict/network";
 import ScrollToTop from "../components/scroll/ScrollToTop";
 
@@ -24,6 +30,30 @@ export default function App() {
         <Route
           path="/training/cisco/ccnp/enterprise"
           element={<CCNPEnterprise />}
+        />
+        <Route
+          path="/training/cisco/ccnp/security"
+          element={<CCNPSecurity />}
+        />
+        <Route
+          path="/training/cisco/ccnp/datacenter"
+          element={<CCNPDatacenter />}
+        />
+        <Route
+          path="/training/cisco/ccnp/ccnp-security/300-710-SNCF-v1.1"
+          element={<SNCF />}
+        />
+        <Route
+          path="/training/cisco/ccnp/ccnp-security/300-715-SISE-v1.1"
+          element={<SISE />}
+        />
+          <Route
+            path="/training/cisco/ccnp/ccnp-security/300-730-SVPN-v1.1"
+            element={<SVPN />}
+          />
+        <Route
+          path="/training/cisco/ccnp/ccnp-security/300-745-SDSI-v1.1"
+          element={<SDSI />}
         />
         <Route path="/solutions/ict/network" element={<Network />} />
         <Route path="*" element={<NotFound />} />
