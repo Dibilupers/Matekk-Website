7ad590b589f9c4bcf1599327e736f7a7a4b0b9d2
adryandevi <doggerman33@example.com>
2026-03-12 11:06:16 +0800

    Training Finished Fortinet Page:
    NSE1
    
    NSE2
    
    NSE3
    
    NSE4
    
    NSE5 Cloud Security
    
    NSE5 SASE
    
    NSE5 Secure Networking
    
    NSE5 Security Operations
    
    Refactor code structure for improved readability and maintainability

---
+++ src/pages/training/fortinet/nse1.jsx
@@ -0,0 +1,305 @@
+import Button from "../../../components/ui/button";
+import { FaCircleCheck } from "react-icons/fa6";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+
+import ccnaBadge from "../../../assets/FortinetNSE1.png";
+import ccnaHero from "../../../assets/CCNAHeroImageAutomation.png";
+import ccnaFooter from "../../../assets/CCNAFooterImageAutomation.png";
+import MealIcon from "../../../assets/Meal.svg";
+import ReviewIcon from "../../../assets/Review.svg";
+import InstitutionalIcon from "../../../assets/InsitutionalAccount.svg";
+import WorkstationIcon from "../../../assets/Workstation.svg";
+import MedalIcon from "../../../assets/Medal.svg";
+import CourseModules from "../../../components/training/courseModules";
+import TrainingTemplateFooter from "../../../components/section/TrainingTemplateFooter";
+import EnrollNowButton from "../../../components/ui/EnrollNowButton";
+
+// ==================== COMPONENT NAME - CHANGE FOR EACH COURSE ====================
+export default function NSE1() {
+  // ==================== COURSE MODULES - UPDATE CONTENT FOR EACH COURSE ====================
+ const modules = [
+  {
+    title: "1.0 Cybersecurity",
+    items: [
+      "1.1 Identify and describe vulnerabilities and exploits",
+      {
+        subTitle: "1.2 Identify and describe the stages of the cyber attack lifecycle",
+        items: [
+          "1.2.1 Reconnaissance",
+          "1.2.2 Weaponization and Delivery",
+          "1.2.3 Exploitation",
+          "1.2.4 Installation",
+          "1.2.5 Command-and-Control (C2)",
+          "1.2.6 Actions on the Objective",
+        ],
+      },
+      "1.3 Identify and describe common attack types (e.g., malware, insider threat, C2, social engineering, AI-powered)",
+      {
+        subTitle: "1.4 Identify and describe intrusion detection systems",
+        items: [
+          "1.4.1 Intrusion Detection System (IDS)",
+          "1.4.2 Host-Based Intrusion Detection System (HIDS)",
+          "1.4.3 Network-Based Intrusion Detection System (NIDS)",
+        ],
+      },
+      "1.5 Identify and describe threat prevention systems and practices (e.g., end user awareness, security updates, antivirus, intrusion prevention systems, firewalls)",
+      "1.6 Explain the purpose of a DMZ",
+      "1.7 Explain the purpose of Zero Trust",
+    ],
+  },
+];
+
+  return (
+    <main className="flex flex-col h-full mx-[2.063rem] md:mx-16 lg:mx-16 xl:mx-[7.438rem] scroll-mt-18 space-y-10">
+      {/* ==================== HERO BANNER ====================*/}
+      <section className="relative pl-8 py-18 rounded-2xl shadow-lg flex flex-col gap-3 overflow-hidden min-h-62.5">
+        {/* ==================== HERO BANNER - Image ====================*/}
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url(${ccnaHero})`,
+          }}
+        />
+        {/* Hero Background Image Gradient */}
+        <div className="absolute inset-0 bg-linear-to-r from-[#cbddf5] via-[#cbddf5ce] to-transparent"></div>
+
+        {/* ==================== HERO BANNER - Text Content ====================*/}
+        <div className="relative z-10 flex flex-col gap-3 pl-1 sm:pl-8">
+          {/* Course Category */}
+          <h6 className="w-fit px-4 py-2 text-[#1775EE] bg-blue-50 rounded-full">
+            FORTINET
+          </h6>
+
+          {/* ====== Course Title and Description ======*/}
+          <div>
+            {/* Course title */}
+            <h1 className="text-[#1775EE] font-bold">
+              NSE 1
+            </h1>
+
+            {/* Course description */}
+            <p className="lg:w-[30vw] xl:w-lg text-gray-900">
+              In this course, you will explore the cyberthreat landscape, including common network threats,
+              the attackers behind them, and the basic cybersecurity principles that help keep you and your network secure.
+              This course serves as an introduction for beginners in cybersecurity, aligned with Fortinet NSE 1 fundamentals.
+            </p>
+          </div>
+
+          {/* ====== Hero Button ====== */}
+          <div className="flex flex-row sm:flex-row gap-2.5 mt-1">
+            {/* Enroll Now Button - When clicked, opens a modal */}
+            <EnrollNowButton />
+
+            {/* Learn More Button - When clicked will scroll down to proceed on the other sections of the page */}
+            <Button
+              title="Learn More"
+              type="none"
+              buttonCustomStyle="bg-transparent text-blue-600 border-2 border-blue-600 hover:bg-white hover:border-white hover:text-blue-600 transition ease-in-out transition-all duration-300 px-6 py-3"
+              btnFunc={() => {
+                document
+                  .getElementById("services")
+                  ?.scrollIntoView({ behavior: "smooth", block: "start" });
+              }}
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* ==================== CONTENT SECTION - Training Informations/Content ==================== */}
+      <div className="flex flex-col lg:flex-row justify-start items-start lg:items-stretch gap-6 lg:gap-8">
+        {/* ==================== LEFT SIDE: Contents/Information ==================== */}
+        <div className="flex flex-col gap-8">
+          {/* ==================== ABOUT SECTION ==================== */}
+          <section className="flex flex-col gap-y-3">
+            {/* About Section - Title */}
+            <h3>
+              About This <span className="text-[#1775EE]">Course</span>
+            </h3>
+            {/* About Section - Description */}
+            {/* CHANGE: Course description paragraphs */}
+            <p>
+              In this course, you will learn about the fundamentals of network security and the 
+              cyberthreat landscape. It introduces common cyber threats, basic security concepts, 
+              and the technologies used to protect networks and data.
+            </p>
+            <p>
+              This course is designed as an entry point for anyone interested in cybersecurity and
+              follows the foundational principles of Fortinet NSE 1. You can build on these concepts
+              to explore more advanced cybersecurity topics and certifications.
+            </p>
+          </section>
+
+          {/* ==================== WHAT YOU WILL LEARN ====================*/}
+          <section className="flex flex-col gap-y-5">
+            {/* What You Will Learn Section - Title */}
+            <h3>
+              What You Will <span className="text-[#1775EE]">Learn</span>
+            </h3>
+
+            {/* What You Will Learn Section - List */}
+            <div className="grid flex-col lg:flex-col-2 gap-4">
+              <div className="flex items-center gap-2.5">
+                <FaCircleCheck className="text-[#1775EE] shrink-0 w-6 h-6" />
+                <p>Explain cybersecurity and the core principles of protecting information.</p>
+              </div>
+              <div className="flex items-center gap-2.5">
+                <FaCircleCheck className="text-[#1775EE] shrink-0 w-6 h-6" />
+                <p>Recognize types of cyber threat actors, their tactics, and the defenses used against them</p>
+              </div>
+              <div className="flex items-center gap-2.5">
+                <FaCircleCheck className="text-[#1775EE] shrink-0 w-6 h-6" />
+                <p>Outline various social engineering methods</p>
+              </div>
+              <p className="flex items-center gap-2.5">
+                <FaCircleCheck className="text-[#1775EE] shrink-0 w-6 h-6" />
+                <span>Explain malware categories and how they are delivered</span>
+              </p>
+            </div>
+          </section>
+
+          {/* ==================== COURSE CONTENT ==================== */}
+          <section className="flex flex-col gap-y-5">
+            {/* Course Content - Title */}
+            <h3>
+              Course <span className="text-[#1775EE]">Content</span>
+            </h3>
+
+            {/* Course Content - List */}
+            <CourseModules modules={modules} />
+          </section>
+        </div>
+
+        {/* ==================== RIGHT SIDEBAR: Contents/Information ==================== */}
+        <aside className="flex flex-col gap-5 lg:w-72 xl:w-80 shrink-0">
+          {/* ==================== CERT BADGE ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex items-center justify-center h-64">
+            {/* Cert Badge - Change/Update Image */}
+            <img
+              src={ccnaBadge}
+              alt="Fortinet NSE 1 Certification Badge"
+              className="w-50 h-50 object-contain"
+            />
+          </div>
+
+          {/* ==================== JOB OPPORTUNITIES ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
+            {/* Job Opportunities - Title */}
+            <h5 className="text-[#1775EE] font-bold">Job Opportunities</h5>
+            {/* Job Opportunities - List */}
+            {[
+              "IT Support Technician",
+              "Cybersecurity Intern",
+              "Network Administrator",
+              "Security Analyst",
+            ].map((job) => (
+              <p key={job} className="flex gap-2 justify-left items-center">
+                <IoIosCheckmarkCircleOutline className="text-[#1775EE] shrink-0 w-6 h-6 mt-0.5" />
+                <span className="text-sm">{job}</span>
+              </p>
+            ))}
+          </div>
+
+          {/* ==================== TARGET LEARNERS ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
+            {/* Target Learners - Title */}
+            <h5 className="text-[#1775EE] font-bold">Target Learners</h5>
+            {/* Target Learners - List */}
+            {["Beginners"].map((job) => (
+              <p key={job} className="flex gap-2 justify-left items-center">
+                <IoIosCheckmarkCircleOutline className="text-[#1775EE] shrink-0 w-6 h-6 mt-0.5" />
+                <span className="text-sm">{job}</span>
+              </p>
+            ))}
+          </div>
+
+          {/* ==================== SCHEDULE ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
+            {/* Schedule - Title */}
+            <h5 className="text-[#1775EE] font-bold">Schedule</h5>
+            {/* Schedule - List */}
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex justify-start gap-2">
+                <p className="font-bold">Modality:</p>
+                <p>Face to Face/Online</p>
+              </div>
+              <div className="flex justify-start gap-2">
+                <p className="font-bold">Duration:</p>
+                <p>7 Days</p>
+              </div>
+              <div className="flex justify-start gap-2">
+                <p className="font-bold">Days:</p>
+                <p>Monday-Sunday</p>
+              </div>
+              <div className="flex justify-start gap-2">
+                <span className="font-bold">Time:</span>
+                <span>10:00 AM – 6:00 PM</span>
+              </div>
+            </div>
+          </div>
+
+          {/* ==================== INCLUSIONS ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
+            {/* Inlcusions - Title */}
+            <h5 className="text-[#1775EE] font-bold">Inclusions</h5>
+
+            {/* Inclusions - List */}
+            <div className="inclusions">
+              <span className="inclusions_icons">
+                <img src={MealIcon} alt="Meal" className="w-4 h-4" />
+              </span>
+              <p className="text-sm">Lunch & snacks (onsite)</p>
+            </div>
+
+            <div className="inclusions">
+              <span className="inclusions_icons">
+                <img src={ReviewIcon} alt="Review" className="w-4 h-4" />
+              </span>
+              <p className="text-sm">Reviewers & exercises</p>
+            </div>
+
+            <div className="inclusions">
+              <span className="inclusions_icons">
+                <img
+                  src={InstitutionalIcon}
+                  alt="Institutional Account"
+                  className="w-4 h-4"
+                />
+              </span>
+              <p className="text-sm">Institutional Accounts</p>
+            </div>
+
+            <div className="inclusions">
+              <span className="inclusions_icons">
+                <img
+                  src={WorkstationIcon}
+                  alt="Workstation"
+                  className="w-4 h-4"
+                />
+              </span>
+              <p className="text-sm">Workstation per student</p>
+            </div>
+
+            <div className="inclusions">
+              <span className="inclusions_icons">
+                <img src={MedalIcon} alt="Medal" className="w-4 h-4" />
+              </span>
+              <p className="text-sm">Certificate of Completion</p>
+            </div>
+
+            {/* CHANGE: Download link text and URL */}
+            <a href="#" className="text-sm font-medium underline mt-2">
+              Download the Palo Alto Exam Basics here
+            </a>
+          </div>
+        </aside>
+      </div>
+
+      {/* ==================== CALL TO ACTION BANNER - UPDATE IMAGE ====================*/}
+      <TrainingTemplateFooter
+        bgImage={ccnaFooter} // CHANGE: Update background image for the footer banner
+        description="Advance your networking career and prepare for the CCNP Enterprise exam."
+      />
+    </main>
+  );
+}
