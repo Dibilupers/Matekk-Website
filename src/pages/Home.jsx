36f8acd1f385f6acf2233d26a3de909133600ce1
faith <faithcalma07@gmail.com>
2026-03-13 12:55:59 +0800

    Fixed Our Working Process section responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -861,17 +861,17 @@ export default function Home() {
                 viewBox="0 0 1200 500"
                 preserveAspectRatio="none"
               >
-                <path d="M -80 30 C 120 100, 200 1, 335 110" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
-                <path d="M 350 120 C 430 130, 500 190, 600 190" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
-                <path d="M 600 215 C 600 160, 800 200, 860 240" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
-                <path d="M 800 250 C 800 150, 845 350, 1500 275" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
+                <path className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.4)]" d="M 0 30 C 250 80, 190 50, 320 120" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
+                <path className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.4)]" d="M 365 115 C 220 130, 500 190, 600 190" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
+                <path className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.4)]" d="M 590 215 C 600 160, 800 200, 860 240" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
+                <path className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.4)]" d="M 847 250 C 800 150, 845 350, 1500 275" stroke="#3B82F6" strokeWidth="8" fill="none" strokeLinecap="round" />
               </svg>
               <div className="relative z-10 flex items-start justify-between gap-4 pb-8">
                 {/* Step 1 */}
                 <div className="desktop_process_steps">
                   <div className="desktop_process_icons">
                     <svg className="desktop_process_svg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20h3v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                      <path  strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20h3v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                   </div>
                   <div className="space-y-1 text-center">
