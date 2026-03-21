74d1ceaca8f8a2859db6325ebf44e7563958e4da
adryandevi <doggerman33@example.com>
2026-03-21 14:42:53 +0800

    Vite PWA Update

--- vite.config.js
+++ vite.config.js
@@ -14,25 +14,6 @@ export default defineConfig({
     
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Matekk Cybercore Academy',
-        short_name: 'Matekk Cybercore Academy',
-        description: 'Matekk Cybercore Academy Training and ICT Services',
-        theme_color: '#1775EE',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
       workbox: {
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg,gif,woff,woff2}'],
