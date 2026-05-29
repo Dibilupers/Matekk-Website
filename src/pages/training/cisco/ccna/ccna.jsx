4755fe1928a40cf7e94d8bf875611015cd4be8fa
faith <faithcalma07@gmail.com>
2026-05-29 23:44:34 +0800

    feat: update job titles to specify "Recent IT/CS/Engineering Graduates" in CCNA Automation and Cybersecurity pages

--- src/pages/training/cisco/ccna/ccna.jsx
+++ src/pages/training/cisco/ccna/ccna.jsx
@@ -255,7 +255,7 @@ export default function CCNA() {
             {[
               "Entry-level IT Professionals",
               "Career Shifters", 
-              "IT/CS/Engineering Graduates",
+              "Recent IT/CS/Engineering Graduates",
             ].map((job) => (
               <p key={job} className="flex gap-2 justify-left items-center">
                 <IoIosCheckmarkCircleOutline className="text-[#1775EE] shrink-0 w-6 h-6 mt-0.5" />
