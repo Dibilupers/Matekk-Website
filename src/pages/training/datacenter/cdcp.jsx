afaaa35683fc163e8b33111e52fbd7ca1dc11931
adryandevi <doggerman33@gmail.com>
2026-09-08 23:47:46 +0800

    feat: add prerequisites section to CDCP component for clarity

--- src/pages/training/datacenter/cdcp.jsx
+++ src/pages/training/datacenter/cdcp.jsx
@@ -315,6 +315,24 @@ export default function CDCP() {
             ))}
           </div>
 
+          {/* ==================== PREREQUISITES ====================*/}
+          <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
+            {/* Prerequisites - Title */}
+            <h5 className="text-[#1775EE] ">Prerequisites</h5>
+            {/* Prerequisites - List */}
+            {[
+              "Beginners are welcome",
+              "No prior experience is required",
+              "No specific prerequisites required",
+              "1–2 years of relevant experience recommended"
+            ].map((item) => (
+              <p key={item} className="flex gap-2 justify-left items-center">
+                <IoIosCheckmarkCircleOutline className="text-[#1775EE] shrink-0 w-6 h-6 mt-0.5" />
+                <span>{item}</span>
+              </p>
+            ))}
+          </div>
+
           {/* ==================== SCHEDULE ====================*/}
           <div className="p-6 rounded-2xl bg-[#EBF5FD] flex flex-col gap-2">
             {/* Schedule - Title */}
